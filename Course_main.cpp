1b148bc3316aeebf050e16f38c9ab5af8c3b72df
EvenDebauge <74670046+EvenDebauge@users.noreply.github.com>
2021-03-10 16:40:29 +0100

    Add files via upload

---
+++ Course_main.cpp
@@ -0,0 +1,50 @@
+#include "Course.h"
+#include "Course.cpp"
+#include <string>
+#include <iostream>
+
+    int main() {
+        
+        Course Joueur1 Course ("1");
+        Joueur1.getPseudoJoueur();
+        std::cout << "/ / / / / /" << std::endl;
+        Joueur1.getMoyenne();
+        Joueur1.getMeilleurScore();
+        Joueur1.getAncienScore();
+        Joueur1.getNouveauScore();
+        Joueur1.getCourse();
+        Joueur1.getScoreNegatif();
+
+        Course Joueur2 Course ("1");
+        Joueur2.getPseudoJoueur();
+        std::cout << "/ / / / / /" << std::endl;
+        Joueur2.getMoyenne();
+        Joueur2.getMeilleurScore();
+        Joueur2.getAncienScore();
+        Joueur2.getNouveauScore();
+        Joueur2.getCourse();
+        Joueur2.getScoreNegatif();
+
+          Course Joueur3 Course ("1");
+        Joueur3.getPseudoJoueur();
+        std::cout << "/ / / / / /" << std::endl;
+        Joueur3.getMoyenne();
+        Joueur3.getMeilleurScore();
+        Joueur3.getAncienScore();
+        Joueur3.getNouveauScore();
+        Joueur3.getCourse();
+        Joueur3.getScoreNegatif();
+
+          Course Joueur4 Course ("1");
+        Joueur4.getPseudoJoueur();
+        std::cout << "/ / / / / /" << std::endl;
+        Joueur4.getMoyenne();
+        Joueur4.getMeilleurScore();
+        Joueur4.getAncienScore();
+        Joueur4.getNouveauScore();
+        Joueur4.getCourse();
+        Joueur4.getScoreNegatif();
+
+      
+        
+    };
